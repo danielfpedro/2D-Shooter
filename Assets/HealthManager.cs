f764919986d920f2d112ab587474e4ffa92525eb
danielfpedro <danielfpedro@gmail.com>
2018-10-18 01:37:49 -0300

    coisas

--- Assets/HealthManager.cs
+++ Assets/HealthManager.cs
@@ -9,17 +9,49 @@ public class HealthManager : MonoBehaviour
 
     public Animator animator;
 
+    private int nextIndex = 0;
+
     public ParticleSystem hitEffect;
+
+    public float moveRate = 2f;
+    private float nextMove;
+
+    NesScripts.Controls.PathFind.Grid grid;
+    NesScripts.Controls.PathFind.Point _from;
+    NesScripts.Controls.PathFind.Point _to;
 
     // Start is called before the first frame update
     void Start()
     {
         currentHealth = maxHealth;
+
+        bool[,] tilesmap = new bool[100, 100];
+        for (int i = 0; i < 100; i++)
+        {
+            for (int y = 0; y < 100; y++)
+            {
+                tilesmap[i, y] = true;
+            }
+        }
+
+        grid = new NesScripts.Controls.PathFind.Grid(tilesmap);
+
+        _from = new NesScripts.Controls.PathFind.Point(1, 1);
+        _to = new NesScripts.Controls.PathFind.Point(50, 50);
     }
 
     // Update is called once per frame
     void Update()
     {
+        List<NesScripts.Controls.PathFind.Point> path = NesScripts.Controls.PathFind.Pathfinding.FindPath(grid, _from, _to);
+
+        if (Time.time >= nextMove)
+        {
+            nextMove = Time.time + 1f / moveRate;
+            transform.position = Vector3.Lerp(transform.position, new Vector3(path[nextIndex].x, path[nextIndex].y, transform.position.z), .2f);
+            nextIndex++;
+        }
+
         if (currentHealth <= 0)
         {
             Kill();
--- Assets/WeaponController.cs
+++ Assets/WeaponController.cs
@@ -14,6 +14,17 @@ public class WeaponController : MonoBehaviour
     private SpriteRenderer[] muzzlesVisuals = new SpriteRenderer[3];
     private Light[] lightVisuals = new Light[3];
     public int currentVisualWeaponIndex = 0;
+    private Vector3 rayDestiny;
+
+    public bool canHold = false;
+
+    private AudioSource[] audioSources = new AudioSource[20];
+
+    public AudioSource machineGunStart;
+    public AudioSource machineGunLoop;
+    public AudioSource machineGunEnd;
+    private bool playOutsound = false;
+
     /**
     public SpriteRenderer weaponUp;
     private SpriteRenderer weaponUpMuzzle;
@@ -39,8 +50,46 @@ public class WeaponController : MonoBehaviour
 
     public Vector2 heroPosition;
 
+    private bool playingSound = false;
+
+    private int currentAudioSourceIndex = 0;
+
+    public float reactivateRate;
+    private float nextReactivate;
+
+    public float soundRate;
+    private float nextSoundRate;
+
+    public AudioSource oneShotAudio;
+
+    public bool automatic = false;
+
+    public bool firing;
+
     void Start()
     {
+        /**
+        if (canHold)
+        {
+            audioSources[0] = gameObject.AddComponent<AudioSource>();
+            audioSources[0].clip = audioClip;
+            audioSources[0].loop = true;
+        }
+        else {
+
+        }
+    **/
+
+        // instantiate audiosources on pool
+        /**for (int i = 0; i < audioSources.Length; i++)
+        {
+            audioSources[i] = gameObject.AddComponent<AudioSource>();
+            audioSources[i].clip = audioClip;
+            audioSources[i].loop = false;
+            audioSources[i].pitch = .6f;
+            audioSources[i].priority = Random.Range(1, 100);
+        }**/
+
         for (int i = 0; i < weaponsVisuals.Length; i++)
         {
             muzzlesVisuals[i] = weaponsVisuals[i].transform.GetChild(0).GetComponent<SpriteRenderer>();
@@ -53,18 +102,62 @@ public class WeaponController : MonoBehaviour
 
     void Update()
     {
- 
-        Debug.DrawRay(transform.position, transform.right * 100f);
-        if (Input.GetButtonUp("Fire1"))
+
+        rayDestiny = transform.right;
+        if (currentVisualWeaponIndex == 1)
         {
-            nextFire = Time.time;
-        }
-        if (Input.GetButton("Fire1") && Time.time >= nextFire)
+            rayDestiny = transform.up;
+        } else if (currentVisualWeaponIndex == 2)
         {
-            nextFire = Time.time + 1f / fireRate;
-            // muzzle.enabled = false;
-            StartCoroutine(Shoot());
+            rayDestiny = transform.up * -1f;
         }
+        Debug.DrawRay(weaponsVisuals[currentVisualWeaponIndex].transform.position, rayDestiny * 100f);
+
+        // bool theInput = (automatic) ? Input.GetButton("Fire1") : Input.GetButtonDown("Fire1");
+
+        if (!automatic)
+        {
+            if (Input.GetButtonDown("Fire1") && Time.time >= nextReactivate)
+            {
+                nextReactivate = Time.time + 1f / reactivateRate;
+                firing = true;
+                oneShotAudio.PlayOneShot(oneShotAudio.clip);
+                StartCoroutine(Shoot());
+            }
+            else
+            {
+                firing = false;
+            }
+        }
+        else
+        {
+            if (Input.GetButton("Fire1"))
+            {
+                nextReactivate = Time.time + 1f / reactivateRate;
+                if (!machineGunLoop.isPlaying)
+                {
+                    machineGunLoop.loop = true;
+                    machineGunLoop.Play();
+                }
+
+                if (Time.time >= nextFire)
+                {
+                    nextFire = Time.time + 1f / fireRate;
+                    StartCoroutine(Shoot());
+                }
+                firing = true;
+            }
+
+            if (Input.GetButtonUp("Fire1") && firing)
+            {
+                machineGunEnd.PlayOneShot(machineGunEnd.clip);
+                machineGunLoop.loop = false;
+                firing = false;
+            }
+        }
+
+
+
 
         for (int i = 0; i < weaponsVisuals.Length; i++)
         {
@@ -89,16 +182,9 @@ public class WeaponController : MonoBehaviour
     {
         if (!mute)
         {
-            AudioSource audioSource = gameObject.AddComponent<AudioSource>();
-            audioSource.clip = audioClip;
-            audioSource.volume = .5f;
-            audioSource.pitch = Random.Range(.6f, 1f);
-            audioSource.playOnAwake = false;
-            audioSource.Play();
-            Destroy(audioSource, audioClip.length);
         }
 
-        RaycastHit2D hit = Physics2D.Raycast(transform.position, transform.right, canBeShooted);
+        RaycastHit2D hit = Physics2D.Raycast(weaponsVisuals[currentVisualWeaponIndex].transform.position, rayDestiny, canBeShooted);
 
         if (hit)
         {
